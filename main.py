3073b13a7844dbb94a1a6ef183312f06f7f38cb5
suryanshx18 <mamtaa6012@gmail.com>
2026-09-12 20:58:04 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+import asyncio
+import logging
+from aiogram import Bot, Dispatcher
+from config import BOT_TOKEN
+from database import init_db
+from handlers import router as all_handlers_router
+
+logging.basicConfig(level=logging.INFO)
+
+
+async def main():
+    await init_db()
+    bot = Bot(token=BOT_TOKEN)
+    dp = Dispatcher()
+    dp.include_router(all_handlers_router)
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
